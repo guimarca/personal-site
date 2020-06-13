1af92e0a788c2d327002aceb67dea331b9f1ca58
guillem <guimarca@gmail.com>
2020-06-13 10:01:52 +0200

    fix: github pages asset path

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   // You may only need to add assetPrefix in the production.
-  assetPrefix: '.'
+  assetPrefix: isProd ? '/guimarca.github.io' : '.'
 }
